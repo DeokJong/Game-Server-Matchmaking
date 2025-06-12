9cb737db3bf58c372576c05f72d8b1c71e97fa9f
DeokJong <novelss444@jbnu.ac.kr>
2025-06-12 12:59:27 +0900

    ⚙️CONF: 매치메이킹 서버 초기화

---
+++ match/src/test/java/com/jinjin99/sketch/match/MatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinjin99.sketch.match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
