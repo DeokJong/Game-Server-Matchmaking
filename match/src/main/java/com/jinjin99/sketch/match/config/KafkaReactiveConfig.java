97717124aa9358fdb528566a6f138313ecfe1824
DeokJong <novelss444@jbnu.ac.kr>
2025-06-12 23:34:21 +0900

    ✨FEAT: match에 대한 topic 구독

--- match/src/main/java/com/jinjin99/sketch/match/config/KafkaReactiveConfig.java
+++ match/src/main/java/com/jinjin99/sketch/match/config/KafkaReactiveConfig.java
@@ -1,5 +1,6 @@
 package com.jinjin99.sketch.match.config;
 
+import com.jinjin99.sketch.match.dto.MatchEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.sender.SenderOptions;
 
+import java.util.Collections;
 import java.util.Map;
 
 @Configuration
@@ -17,17 +19,18 @@ public class KafkaReactiveConfig {
   private final KafkaProperties kafkaProperties;
 
   @Bean
-  public ReactiveKafkaProducerTemplate<String, Object> kafkaProducer() {
+  public ReactiveKafkaProducerTemplate<String, MatchEvent> kafkaProducer() {
     Map<String, Object> producerProps = kafkaProperties.buildProducerProperties();
-    SenderOptions<String, Object> senderOptions = SenderOptions.create(producerProps);
+    SenderOptions<String, MatchEvent> senderOptions = SenderOptions.<String, MatchEvent>create(producerProps).maxInFlight(50);
 
     return new ReactiveKafkaProducerTemplate<>(senderOptions);
   }
 
   @Bean
-  public ReactiveKafkaConsumerTemplate<String, Object> kafkaConsumer() {
+  public ReactiveKafkaConsumerTemplate<String, MatchEvent> kafkaConsumer() {
     Map<String, Object> consumerProps = kafkaProperties.buildConsumerProperties();
-    ReceiverOptions<String, Object> receiverOptions = ReceiverOptions.create(consumerProps);
+    ReceiverOptions<String, MatchEvent> receiverOptions = ReceiverOptions.<String, MatchEvent>create(consumerProps)
+            .subscription(Collections.singleton("match"));
 
     return new ReactiveKafkaConsumerTemplate<>(receiverOptions);
   }
